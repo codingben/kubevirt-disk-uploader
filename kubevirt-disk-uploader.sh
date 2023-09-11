be1353940dad836286bf4e8947b065730c0b4272
Ben Oukhanov <boukhanov@redhat.com>
2023-09-11 17:47:14 +0300

    refactor: add output path variable
    Add output path variable in shell script
    to avoid mistakes.
    
    Signed-off-by: Ben Oukhanov <boukhanov@redhat.com>

--- kubevirt-disk-uploader.sh
+++ kubevirt-disk-uploader.sh
@@ -3,7 +3,8 @@
 VM_NAME=$1
 CONTAINER_DISK_NAME=$2
 DISK_FILE=$3
-TEMP_DISK_PATH=tmp/$3
+OUTPUT_PATH=./tmp
+TEMP_DISK_PATH=$OUTPUT_PATH/$3
 REGISTRY_URL=${REGISTRY_HOST}/${REGISTRY_USERNAME}/$CONTAINER_DISK_NAME
 
 function apply_vmexport() {
@@ -39,18 +40,18 @@ function convert_disk_img() {
   echo "Converting raw disk image to qcow2 format..."
 
   gunzip $TEMP_DISK_PATH
-  qemu-img convert -f raw -O qcow2 $TEMP_DISK_PATH tmp/disk.qcow2
+  qemu-img convert -f raw -O qcow2 $TEMP_DISK_PATH $OUTPUT_PATH/disk.qcow2
   rm $TEMP_DISK_PATH
 }
 
 function build_disk_img() {
   echo "Building exported disk image in a new container image..."
 
-  cat << END > tmp/Dockerfile
+  cat << END > $OUTPUT_PATH/Dockerfile
 FROM scratch
 ADD --chown=107:107 ./disk.qcow2 /disk/
 END
-  buildah build -t $CONTAINER_DISK_NAME ./tmp
+  buildah build -t $CONTAINER_DISK_NAME $OUTPUT_PATH
 }
 
 function push_disk_img() {
