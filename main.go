3c0cf6b069e7a4bf57c17a663bf897accba8ce1d
Ben Oukhanov <boukhanov@redhat.com>
2024-01-18 16:35:30 +0200

    refactor: get rid of shell scripting
    Implement all functionality in Golang instead of
    mixing it with shell scripting.
    
    Jira-Url: https://issues.redhat.com/browse/CNV-36060
    
    Signed-off-by: Ben Oukhanov <boukhanov@redhat.com>

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/google/go-containerregistry/pkg/authn"
@@ -18,7 +20,109 @@ import (
 	tar "kubevirt.io/containerdisks/pkg/build"
 )
 
-func build(diskPath string) (v1.Image, error) {
+const (
+	diskPath             string = "./tmp/disk.img.gz"
+	diskPathDecompressed string = "./tmp/disk.img"
+	diskPathConverted    string = "./tmp/disk.qcow2"
+)
+
+func applyVirtualMachineExport(vmName string) error {
+	log.Println("Applying VirtualMachineExport object...")
+
+	yaml := fmt.Sprintf(`
+apiVersion: export.kubevirt.io/v1alpha1
+kind: VirtualMachineExport
+metadata:
+  name: %s
+spec:
+  source:
+    apiGroup: "kubevirt.io"
+    kind: VirtualMachine
+    name: %s
+`, vmName, vmName)
+	cmd := exec.Command("kubectl", "apply", "-f", "-")
+	cmd.Stdin = strings.NewReader(yaml)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func downloadVirtualMachineDiskImage(vmName, volumeName string) error {
+	log.Printf("Downloading disk image from %s Virtual Machine...\n", vmName)
+
+	cmd := exec.Command("usr/bin/virtctl", "vmexport", "download", vmName, "--vm", vmName, "--volume", volumeName, "--output", diskPath)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	if fileInfo, err := os.Stat(diskPath); err != nil || fileInfo.Size() == 0 {
+		return fmt.Errorf("File does not exist or is empty.")
+	}
+
+	log.Println("Download completed successfully.")
+	return nil
+}
+
+func decompressVirtualMachineDiskImage() error {
+	log.Println("Decompressing downloaded disk image...")
+
+	cmd := exec.Command("gunzip", diskPath)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	log.Println("Decompressed completed successfully.")
+	return nil
+}
+
+func convertRawDiskImageToQcow2() error {
+	log.Println("Converting raw disk image to qcow2 format...")
+
+	cmd := exec.Command("qemu-img", "convert", "-f", "raw", "-O", "qcow2", diskPathDecompressed, diskPathConverted)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	if fileInfo, err := os.Stat(diskPathConverted); err != nil || fileInfo.Size() == 0 {
+		return fmt.Errorf("Converted file does not exist or is empty.")
+	}
+
+	log.Println("Conversion to qcow2 format completed successfully.")
+	return nil
+}
+
+func prepareVirtualMachineDiskImage(enableVirtSysprep bool) error {
+	if enableVirtSysprep {
+		log.Println("Preparing disk image...")
+
+		os.Setenv("LIBGUESTFS_BACKEND", "direct")
+		cmd := exec.Command("virt-sysprep", "--format", "qcow2", "-a", diskPathConverted)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		if err := cmd.Run(); err != nil {
+			return err
+		}
+	} else {
+		log.Println("Skipping disk image preparation.")
+	}
+	return nil
+}
+
+func buildContainerDisk(diskPath string) (v1.Image, error) {
 	layer, err := tarball.LayerFromOpener(tar.StreamLayerOpener(diskPath))
 	if err != nil {
 		log.Fatalf("Error creating layer from file: %v", err)
@@ -35,17 +139,19 @@ func build(diskPath string) (v1.Image, error) {
 	return image, nil
 }
 
-func push(image v1.Image, imageDestination string) error {
+func pushContainerDisk(image v1.Image, containerDiskName string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*30)
 	defer cancel()
 
-	username := os.Getenv("REGISTRY_USERNAME")
+	hostName := os.Getenv("REGISTRY_HOST")
+	userName := os.Getenv("REGISTRY_USERNAME")
 	password := os.Getenv("REGISTRY_PASSWORD")
 	auth := &authn.Basic{
-		Username: username,
+		Username: userName,
 		Password: password,
 	}
 
+	imageDestination := fmt.Sprintf("%s/%s/%s", hostName, userName, containerDiskName)
 	err := crane.Push(image, imageDestination, crane.WithAuth(auth), crane.WithContext(ctx))
 	if err != nil {
 		log.Fatalf("Error pushing image: %v", err)
@@ -56,27 +162,65 @@ func push(image v1.Image, imageDestination string) error {
 	return nil
 }
 
+func run(vmName, volumeName, containerDiskName string, enableVirtSysprep bool) error {
+	if err := applyVirtualMachineExport(vmName); err != nil {
+		return err
+	}
+
+	if err := downloadVirtualMachineDiskImage(vmName, volumeName); err != nil {
+		return err
+	}
+
+	if err := decompressVirtualMachineDiskImage(); err != nil {
+		return err
+	}
+
+	if err := convertRawDiskImageToQcow2(); err != nil {
+		return err
+	}
+
+	if err := prepareVirtualMachineDiskImage(enableVirtSysprep); err != nil {
+		return err
+	}
+
+	image, err := buildContainerDisk(diskPathConverted)
+	if err != nil {
+		return err
+	}
+
+	return pushContainerDisk(image, containerDiskName)
+}
+
 func main() {
-	var diskPath string
-	var imageDestination string
+	var vmName string
+	var volumeName string
+	var containerDiskName string
+	var enableVirtSysprep bool
 
 	var command = &cobra.Command{
 		Use:   "kubevirt-disk-uploader",
 		Short: "Extracts disk and uploads it to a container registry",
 		Run: func(cmd *cobra.Command, args []string) {
-			if image, err := build(diskPath); err == nil {
-				push(image, imageDestination)
+			log.Println("Extracts disk and uploads it to a container registry...")
+
+			if err := run(vmName, volumeName, containerDiskName, enableVirtSysprep); err != nil {
+				log.Panicln(err)
 			}
+
+			log.Println("Succesfully extracted disk image and uploaded it in a new container image to container registry.")
 		},
 	}
 
-	command.Flags().StringVarP(&diskPath, "diskpath", "d", "", "path to the disk")
-	command.Flags().StringVarP(&imageDestination, "imagedestination", "i", "", "destination of the image")
-	command.MarkFlagRequired("diskpath")
-	command.MarkFlagRequired("imagedestination")
+	command.Flags().StringVar(&vmName, "vmname", "", "name of the virtual machine")
+	command.Flags().StringVar(&volumeName, "volumename", "", "volume name of the virtual machine")
+	command.Flags().StringVar(&containerDiskName, "containerdiskname", "", "name of the new container image")
+	command.Flags().BoolVar(&enableVirtSysprep, "enablevirtsysprep", false, "enable or disable virt-sysprep")
+	command.MarkFlagRequired("vmname")
+	command.MarkFlagRequired("volumename")
+	command.MarkFlagRequired("containerDiskName")
 
 	if err := command.Execute(); err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		os.Exit(1)
 	}
 }
