c25ac58c5bbfa3ca173faccf1cd22f65f6a0003f
Ben Oukhanov <boukhanov@redhat.com>
2024-08-18 18:05:22 +0300

    chore: bump golang to 1.22.4
    Signed-off-by: Ben Oukhanov <boukhanov@redhat.com>

--- go.mod
+++ go.mod
@@ -1,28 +1,27 @@
 module github.com/codingben/kubevirt-disk-uploader
 
-go 1.21.4
+go 1.22.4
 
 require (
-	github.com/google/go-containerregistry v0.16.1
-	github.com/spf13/cobra v1.7.0
-	kubevirt.io/containerdisks v0.0.0-20230713082819-f3649ff361ed
+	github.com/google/go-containerregistry v0.20.2
+	github.com/spf13/cobra v1.8.1
+	kubevirt.io/containerdisks v0.0.0-20240815082608-c88d3cc649e2
 )
 
 require (
-	github.com/containerd/stargz-snapshotter/estargz v0.14.3 // indirect
-	github.com/docker/cli v24.0.0+incompatible // indirect
-	github.com/docker/distribution v2.8.2+incompatible // indirect
-	github.com/docker/docker v24.0.0+incompatible // indirect
-	github.com/docker/docker-credential-helpers v0.7.0 // indirect
+	github.com/containerd/stargz-snapshotter/estargz v0.15.1 // indirect
+	github.com/docker/cli v27.1.2+incompatible // indirect
+	github.com/docker/distribution v2.8.3+incompatible // indirect
+	github.com/docker/docker-credential-helpers v0.8.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/klauspost/compress v1.16.5 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
-	github.com/opencontainers/image-spec v1.1.0-rc3 // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/sirupsen/logrus v1.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/vbatts/tar-split v0.11.3 // indirect
-	golang.org/x/sync v0.2.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
+	github.com/vbatts/tar-split v0.11.5 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
 )
--- vendor/kubevirt.io/containerdisks/pkg/build/build.go
+++ vendor/kubevirt.io/containerdisks/pkg/build/build.go
@@ -6,21 +6,48 @@ import (
 	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/empty"
 	"github.com/google/go-containerregistry/pkg/v1/mutate"
+	"github.com/google/go-containerregistry/pkg/v1/partial"
 	"github.com/google/go-containerregistry/pkg/v1/tarball"
+	"github.com/google/go-containerregistry/pkg/v1/types"
 )
 
 const (
-	LabelShaSum       = "shasum"
-	ImageArchitecture = "amd64"
+	LabelShaSum = "shasum"
+	ImageOS     = "linux"
 )
 
-func ContainerDisk(imgPath string, checksum string) (v1.Image, error) {
-	img := empty.Image
+func ContainerDiskConfig(checksum string, envVariables map[string]string) v1.Config {
+	labels := map[string]string{
+		LabelShaSum: checksum,
+	}
+
+	var env []string
+	for k, v := range envVariables {
+		env = append(env, fmt.Sprintf("%s=%s", k, v))
+	}
+
+	// OCI runtimes like crun-vm [1] may be able to run a containerdisk container image directly, in which case it
+	// would be unfortunate to require the user to pass in a placeholder entrypoint to docker-run and equivalents,
+	// since engines like Docker require an entrypoint to be set, e.g.:
+	//
+	//   $ docker run --runtime crun-vm quay.io/containerdisks/fedora:39 unused-entrypoint
+	//
+	// Instead we set the entrypoint to a predetermined "no-entrypoint" value that runtimes like crun-vm can
+	// interpret as there not being an entrypoint.
+	//
+	// [1] https://github.com/containers/crun-vm
+	entrypoint := []string{"no-entrypoint"}
+
+	return v1.Config{Labels: labels, Env: env, Entrypoint: entrypoint}
+}
+
+func ContainerDisk(imgPath, imgArch string, config v1.Config) (v1.Image, error) {
 	layer, err := tarball.LayerFromOpener(StreamLayerOpener(imgPath))
 	if err != nil {
 		return nil, fmt.Errorf("error creating an image layer from disk: %v", err)
 	}
 
+	img := mutate.MediaType(empty.Image, types.DockerManifestSchema2)
 	img, err = mutate.AppendLayers(img, layer)
 	if err != nil {
 		return nil, fmt.Errorf("error appending the image layer: %v", err)
@@ -32,8 +59,9 @@ func ContainerDisk(imgPath string, checksum string) (v1.Image, error) {
 	}
 
 	// Modify the config file
-	cf.Architecture = ImageArchitecture
-	cf.Config = v1.Config{Labels: map[string]string{LabelShaSum: checksum}}
+	cf.Architecture = imgArch
+	cf.OS = ImageOS
+	cf.Config = config
 
 	img, err = mutate.ConfigFile(img, cf)
 	if err != nil {
@@ -41,4 +69,29 @@ func ContainerDisk(imgPath string, checksum string) (v1.Image, error) {
 	}
 
 	return img, nil
+}
+
+func ContainerDiskIndex(images []v1.Image) (v1.ImageIndex, error) {
+	var indexAddendum []mutate.IndexAddendum
+
+	for _, image := range images {
+		configFile, err := image.ConfigFile()
+		if err != nil {
+			return nil, err
+		}
+
+		descriptor, err := partial.Descriptor(image)
+		if err != nil {
+			return nil, err
+		}
+		descriptor.Platform = configFile.Platform()
+
+		indexAddendum = append(indexAddendum, mutate.IndexAddendum{
+			Add:        image,
+			Descriptor: *descriptor,
+		})
+	}
+
+	idx := mutate.IndexMediaType(empty.Index, types.DockerManifestList)
+	return mutate.AppendManifests(idx, indexAddendum...), nil
 }
